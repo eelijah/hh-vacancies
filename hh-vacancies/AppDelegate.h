9501bb3bdb19f75bc0b654f3edac81a97946b8ac
Elijah Ponkratenko <eelsajahber@gmail.com>
2015-09-20 17:51:20 +0300

    init

---
+++ hh-vacancies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hh-vacancies
+//
+//  Created by Elijah Ponkratenko on 20/09/15.
+//  Copyright © 2015 Elijah Ponkratenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hh-vacancies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hh-vacancies
+//
+//  Created by Elijah Ponkratenko on 20/09/15.
+//  Copyright © 2015 Elijah Ponkratenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
